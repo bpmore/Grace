dcc4c82a64841eb349721fb31f449ff8c2363e40
bpmore <brentpassmore@gmail.com>
2014-05-10 19:09:56 -0600

    Grace

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Carry' );
+define( 'CHILD_THEME_NAME', 'Grace' );
 define( 'CHILD_THEME_URL', 'http://bpmo.re/' );
 define( 'CHILD_THEME_VERSION', '1.0' );
 
@@ -16,9 +16,13 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Add new featured image size
 //TBD
 
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before_header', 'genesis_do_nav' );
+
 //* Add Featured Image to Page and Post Above Title
-add_action ( 'genesis_entry_header', 'carry_featured_image_title_singular', 9 );
-function carry_featured_image_title_singular() {
+add_action ( 'genesis_entry_header', 'grace_featured_image_title_singular', 9 );
+function grace_featured_image_title_singular() {
 
 	if ( !is_singular() || !has_post_thumbnail() )
 		return;
@@ -30,21 +34,21 @@ function carry_featured_image_title_singular() {
 }
 
 //* Add Previous and Next Navigation to Post
-add_action('genesis_entry_footer', 'carry_custom_pagination_links', 15 );
-function carry_custom_pagination_links() {
+add_action('genesis_entry_footer', 'grace_custom_pagination_links', 15 );
+function grace_custom_pagination_links() {
 if( !is_single() )
       return;
 
-    previous_post_link('<div class="single-post-nav">&laquo; %link', ' ' . get_previous_post_link('%title') , FALSE);
-    echo ' —/— ';
-    next_post_link('%link  &raquo;</div>', get_next_post_link('%title') . ' ' , FALSE);
+    previous_post_link('<div class="single-post-nav">&#10092; %link', ' ' . get_previous_post_link('%title') , FALSE);
+    echo ' &mdash; ';
+    next_post_link('%link &#10093;</div>', get_next_post_link('%title') . ' ' , FALSE);
 }
 
-//add_action('genesis_after_comments', 'custom_post_navigation');
+add_action('genesis_after_comments', 'custom_post_navigation');
 
 //* Enqueue Josefin Slab and Old Standard TT Google fonts
-add_action( 'wp_enqueue_scripts', 'carry_google_fonts' );
-function carry_google_fonts() {
+add_action( 'wp_enqueue_scripts', 'grace_google_fonts' );
+function grace_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat:700', array(), CHILD_THEME_VERSION );
 
@@ -78,7 +82,7 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 }
 
 //* Unregister navigation menus
-remove_theme_support( 'genesis-menus' );
+//remove_theme_support( 'genesis-menus' );
 
 //* Unregister layout settings
 genesis_unregister_layout( 'content-sidebar' );
@@ -88,13 +92,13 @@ genesis_unregister_layout( 'sidebar-content-sidebar' );
 genesis_unregister_layout( 'sidebar-sidebar-content' );
 
 //* Remove the entry header markup
-remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+//remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+//remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+//remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
 //* Modify the WordPress read more link
-add_filter( 'the_content_more_link', 'carry_read_more' );
-function carry_read_more() {
+add_filter( 'the_content_more_link', 'grace_read_more' );
+function grace_read_more() {
 
 	return '<a class="more-link" href="' . get_permalink() . '">Continue Reading</a>';
 
@@ -103,32 +107,37 @@ function carry_read_more() {
 //* Customize the entry meta in the entry footer
 add_filter( 'genesis_post_meta', 'unfiltered_post_meta_filter' );
 function unfiltered_post_meta_filter($post_meta) {
-
-	$post_meta = 'Published on [post_date] by [post_author_posts_link] in [post_categories before=""] [post_edit]';
+	$post_meta = '[post_categories before="" after=""] [post_tags before=""]';
 	return $post_meta;
+}
 
+//Enqueue the Dashicons script
+add_action( 'wp_enqueue_scripts', 'amethyst_enqueue_dashicons' );
+function amethyst_enqueue_dashicons() {
+	wp_enqueue_style( 'amethyst-dashicons-style', get_stylesheet_directory_uri(), array('dashicons'), '1.0' );
 }
 
 //* Remove the header right widget area
-//*unregister_sidebar( 'header-right' );
+unregister_sidebar( 'header-right' );
 
 //* Unregister secondary sidebar
 unregister_sidebar( 'sidebar-alt' );
 unregister_sidebar( 'sidebar' );
 
 //* Customize entry meta in the entry header
-//add_filter( 'genesis_post_info', 'carry_post_info_filter' );
-//function carry_post_info_filter($post_info) {
-//	$post_info = '[post_date] [post_edit]';
-//	return $post_info;
-//}
+add_filter( 'genesis_post_info', 'grace_post_info_filter' );
+function grace_post_info_filter($post_info) {
+	$post_info = '[post_date] [post_edit]';
+	return $post_info;
+}
+
 
 //* Remove the breadcrumb
 remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
 //* Change the footer text
-add_filter('genesis_footer_creds_text', 'carry_footer_creds_filter');
-function carry_footer_creds_filter( $creds ) {
+add_filter('genesis_footer_creds_text', 'grace_footer_creds_filter');
+function grace_footer_creds_filter( $creds ) {
 	$creds = '[footer_copyright] &middot; <a href="http://brentpassmore.com">Brent Passmore</a>';
 	return $creds;
 }
