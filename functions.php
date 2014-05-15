7a6abb2668e3109ac8c74a57da208b6fe853e02c
bpmore <brentpassmore@gmail.com>
2014-05-15 16:32:51 -0600

    Portfolio tweaks

--- functions.php
+++ functions.php
@@ -255,3 +255,16 @@ function grace_portfolio_items( $query ) {
 	}
 
 }
+
+add_action( 'init', 'cd_add_editor_styles' );
+/**
+* Apply theme's stylesheet to the visual editor.
+*
+* @uses add_editor_style() Links a stylesheet to visual editor
+* @uses get_stylesheet_uri() Returns URI of theme stylesheet
+*/
+function cd_add_editor_styles() {
+ 
+add_editor_style( get_stylesheet_uri() );
+ 
+}
